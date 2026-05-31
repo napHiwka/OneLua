fa220d60c9fbe07395323f1821ce590a2ab2f8c3
napHiwka <napHiwka@gmail.com>
2026-05-31 15:44:47 +0300

    style: remove unnecessary comments and extra lines

--- bare.config.lua
+++ bare.config.lua
@@ -1,16 +1,14 @@
 return {
-	entry = "init",
+	entry = "init.lua",
 	src = "./",
 	out = "./bundle.lua",
 	name = "meow",
 	extra = {
-		"plugins.json_backend",
-		"plugins.xml_backend",
+		"plugins.backend",
 	},
 	skip_extra_files_requires = false,
 	aliases = {
 		["json"] = "vendor.json",
-		["config"] = "app.config.production",
 	},
 	strip = "all",
 	resolve = true,
